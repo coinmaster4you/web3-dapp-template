052a8708413d7cf5210637f7f070325e5a822181
coinmaster4you <5037396+coinmaster4you@users.noreply.github.com>
2021-01-04 05:35:54 +0100

    movement of sub project out of repository

--- web3-wasam/test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import {assert, fail, assertEquals} from "https://deno.land/std/testing/asserts.ts";
-import {wasmModule} from "../index";
-Deno.test({
-    name: "addition",
-    fn(): void {
-        assertEquals(wasmModule.add(1, 2), 3);
-        console.log("ok");
-    },
-  });
